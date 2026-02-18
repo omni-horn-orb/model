df04d2c028fd8d636632bf28d8a8f534cb7a012e
Christopher M. Gibson <79chris.gibson@gmail.com>
2026-02-18 23:47:34 +0000

    Create model_config.py

---
+++ model_config.py
@@ -0,0 +1,11 @@
+# model_config.py
+# Central configuration for Omni-Horn-Orb v2
+
+CONFIG = {
+    "B": 1.0,
+    "c": 0.5,
+    "holonomy_amp": 0.1,
+    "r_scan_min": 0.1,
+    "r_scan_max": 5.0,
+    "r_scan_points": 50
+}

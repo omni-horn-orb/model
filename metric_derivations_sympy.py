d0ffcd983755176178f4b87b90b57b1e460a6cc5
Christopher M. Gibson <79chris.gibson@gmail.com>
2026-02-18 23:56:10 +0000

    Update metric_derivations_sympy.py

--- metric_derivations_sympy.py
+++ metric_derivations_sympy.py
@@ -1,17 +1,20 @@
 # metric_derivations_sympy.py
-# SymPy derivation of the induced metric for the Omni-Horn-Orb hypersphere
+# Omni-Horn-Orb v2 symbolic metric with stabilized radius
 
 import sympy as sp
+from stabilized_radius import r_stable
 
-r, k, v, u, theta = sp.symbols('r k v u theta', real=True, positive=True)
-f = k * (sp.pi - v)
+r = r_stable
+print("Using stabilized radius:", r)
 
-# Induced metric components
-g_vv = r**2 - k**2
-g_uu = r**2 * (1 + sp.cos(v))**2
-g_thetatheta = r**2 * (1 + sp.cos(v))**2 * sp.cos(u)**2
+t, chi = sp.symbols("t chi")
 
-print("g_vv =", g_vv)
-print("g_uu =", g_uu)
-print("g_θθ =", g_thetatheta)
-print("\nLorentzian signature confirmed when k > r")
+a = sp.Function("a")(t)
+
+metric = sp.Matrix([
+    [-1, 0],
+    [0, a**2 * r**2]
+])
+
+print("Metric tensor:")
+sp.pprint(metric)
